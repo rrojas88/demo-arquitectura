8eed4f3b0d0938fca068fcdd835dea2d4ebb8e0f
Robinson Rojas Guarin <robins_r88@hotmail.com>
2022-09-08 19:54:25 -0500

    Consumo api ext por post con headers

--- src/main/java/com/example/demo2/api/v1/external/api_arus/coins/CoinController.java
+++ src/main/java/com/example/demo2/api/v1/external/api_arus/coins/CoinController.java
@@ -1,6 +1,13 @@
 
 package com.example.demo2.api.v1.external.api_arus.coins;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -22,4 +29,52 @@ public class CoinController {
         String result = restTemplate.getForObject(uri, String.class);
         return result;
     }
+    
+    @PostMapping("/test-login")
+    public ResponseEntity<?> postLogin( HttpServletRequest req ) // throws URISyntaxException 
+    {
+        UserCredentials userCredentials = new UserCredentials("pepito", "123456");
+        
+        URI uri = null;
+        try {
+            final String baseUrl = "http://localhost:3000/api/v1/test-login";
+            uri = new URI(baseUrl);
+        } 
+        catch (URISyntaxException e) {
+            System.out.println( "++++++ URISyntaxException" );
+            System.out.println( e.getMessage() );
+        }
+        catch (Exception e) {
+            System.out.println( "++++++ Exception" );
+            System.out.println( e.getMessage() );
+        }
+        System.out.println( "----- URi  " ); System.out.println( uri );
+        
+        String auth_token = req.getHeader("auth-token");
+        if( auth_token == null ) auth_token = "Token x defalt";
+        
+        String public_key = req.getHeader("public-key");
+        if( public_key == null ) public_key = "Sin clave publica";
+        
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("auth-token", auth_token );    
+        headers.set("public-key", public_key );
+        
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        
+        HttpEntity<UserCredentials> requestBody = new HttpEntity<>(userCredentials, headers);
+        
+        ResponseEntity<Object> result = null;
+        try {
+            RestTemplate restTemplate = new RestTemplate();
+            
+            result = restTemplate.postForEntity( uri, requestBody, Object.class );
+        }
+        catch (Exception e) {
+            System.out.println( "++++++ Exception POST:" );
+            System.out.println( e.getMessage() );
+        }
+        return result;
+    }
+    
 }
---
+++ src/main/java/com/example/demo2/api/v1/external/api_arus/coins/UserCredentials.java
@@ -0,0 +1,15 @@
+
+package com.example.demo2.api.v1.external.api_arus.coins;
+
+
+public class UserCredentials {
+    
+    public String username;
+    public String password;
+
+    public UserCredentials(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+    
+}
